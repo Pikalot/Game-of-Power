981e9287129048dc204b05add0faf4a4cc1e7cfd
Bellowsmay <Alimalex1525@gmail.com>
2025-11-10 03:06:59 -0800

    Fixed player jitter. Increased speed.

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends Area2D
 signal hit
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
+@export var speed = 500 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 var touchPos
 var touching
@@ -19,14 +19,15 @@ func start(pos):
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if(touching):
-		if (touchPos.x < position.x):
+		if (touchPos.x < position.x - 50):
 			velocity.x -= 1
-		if (touchPos.x > position.x):
+		if (touchPos.x > position.x + 50):
 			velocity.x += 1
-	if Input.is_action_pressed("move_right") or Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left") or Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
+	if(!touching):
+		if Input.is_action_pressed("move_right") or Input.is_action_pressed("ui_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("move_left") or Input.is_action_pressed("ui_left"):
+			velocity.x -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
