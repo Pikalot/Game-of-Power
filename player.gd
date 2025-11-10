c731978d0b3faff563bdb03e8399ac916a08df0e
Bellowsmay <Alimalex1525@gmail.com>
2025-11-09 20:37:58 -0800

    Created basic player left-right movement

---
+++ player.gd
@@ -0,0 +1,37 @@
+extends Area2D
+signal hit
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right") or Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO + Vector2(100, 0), screen_size - Vector2(100, 0))
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+	
+	
